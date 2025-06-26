29a484b01e537b64fe7ddc79042646369357b612
indra554 <indra_al_rasyid@teknokrat.ac.id>
2025-06-26 23:24:15 +0700

    menambahkan kodingan yang menampilkan daftar buku dengan RecyclerView dan menangani logout pengguna melalui menu.xml

--- app/src/main/java/com/uti/buku_app/BookListActivity.kt
+++ app/src/main/java/com/uti/buku_app/BookListActivity.kt
@@ -1,20 +1,50 @@
 package com.uti.buku_app
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.uti.buku_app.model.Book
 
 class BookListActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_book_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        recyclerView = findViewById(R.id.booksRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = BookAdapter(Book.dummyBooks)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                // Clear user session
+                getSharedPreferences("UserPrefs", MODE_PRIVATE)
+                    .edit()
+                    .clear()
+                    .apply()
+
+                // Return to login screen
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
