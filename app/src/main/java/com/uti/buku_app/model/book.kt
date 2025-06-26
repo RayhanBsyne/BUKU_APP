57ccaf66a9ed07420679662ca2f18a6ed16d462e
indra554 <indra_al_rasyid@teknokrat.ac.id>
2025-06-26 23:01:48 +0700

    menambahkan kodingan untuk list buku yang pertama berdasarkan gambar yang di masukan didrawable

--- app/src/main/java/com/uti/buku_app/model/book.kt
+++ app/src/main/java/com/uti/buku_app/model/book.kt
@@ -1,4 +1,25 @@
 package com.uti.buku_app.model
 
-class book {
+import com.uti.buku_app.R
+
+data class Book(
+    val id: Int,
+    val title: String,
+    val author: String,
+    val description: String,
+    val imageResId: Int,
+    val price: Int
+) {
+    companion object {
+        val dummyBooks = listOf(
+            Book(
+                1,
+                "The Great Gatsby",
+                "F. Scott Fitzgerald",
+                "A story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan.",
+                R.drawable.book_gatsby,
+                250000
+            ),
+        )
+    }
 }
