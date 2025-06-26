e650b677392ab3dab1d7f7e0bbe4e85260b20479
Rayhanbsyne <ada123a7x@gmail.com>
2025-06-27 03:08:57 +0700

    mengedit codingan agar mengecek jika sudah melakukan login maka akan langsung ke menu booklist

--- app/src/main/java/com/uti/buku_app/MainActivity.kt
+++ app/src/main/java/com/uti/buku_app/MainActivity.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE)
+        val username = prefs.getString("username", null)
 
-        val intent = Intent(this, LoginActivity::class.java)
+        val intent = if (username != null) {
+            Intent(this, BookListActivity::class.java)
+        } else {
+            Intent(this, LoginActivity::class.java)
+        }
         startActivity(intent)
         finish()
     }
