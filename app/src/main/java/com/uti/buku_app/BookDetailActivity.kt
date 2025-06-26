8283ffb8062dbe7b0b847aec340e82aea5f67d6c
Rayhanbsyne <ada123a7x@gmail.com>
2025-06-27 02:48:55 +0700

    Merapihkan codingan agar terlihat lebih terstruktur

--- app/src/main/java/com/uti/buku_app/BookDetailActivity.kt
+++ app/src/main/java/com/uti/buku_app/BookDetailActivity.kt
@@ -35,17 +35,14 @@ class BookDetailActivity : AppCompatActivity() {
         // Load book image from drawable
         findViewById<android.widget.ImageView>(R.id.bookImage).setImageResource(book.imageResId)
 
-        // Set book details
         findViewById<android.widget.TextView>(R.id.bookTitle).text = book.title
         findViewById<android.widget.TextView>(R.id.bookAuthor).text = book.author
 
-        // Format price in Indonesian Rupiah
         val formatter = NumberFormat.getCurrencyInstance(Locale("id", "ID"))
         findViewById<android.widget.TextView>(R.id.bookPrice).text = formatter.format(book.price)
 
         findViewById<android.widget.TextView>(R.id.bookDescription).text = book.description
 
-        // Set up WhatsApp order button
         findViewById<MaterialButton>(R.id.orderButton).setOnClickListener {
             val message = "Saya ingin pesan buku ${book.title}"
             val encodedMessage = Uri.encode(message)
